8b3d7d0b5264b748cc1f376de0159da3b10f2fc3
a-bodrova <soulwent94@gmail.com>
2021-08-18 16:23:28 +0400

    feat: add code in functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let arr = Array.from(array);
+  let min = Math.min.apply(null, arr);
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let max = Math.max.apply(null, array);
+
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let arr = Array.from(array);
+  let sum = arr.reduce( (acc, item) => acc + item);
+  let avg = sum / arr.length;
+  return avg;
 }
